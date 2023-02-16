a9a3ce07a579b2caf0437a48291e795e08ca88c6
Vitaliy <vitalik.koliesnikov@gmail.com>
2023-02-16 22:20:52 +0200

    Header issue fixed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.header');
 const iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
     iconMenu.addEventListener("click", function () {
-        document.body.classList.toggle('_lock');
+        document.body.classList.toggle('_lock');   
         let children = header.children;
         console.log(children);
         for(let i=0; i<children.length; i++){
