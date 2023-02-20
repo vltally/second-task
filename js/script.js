7d4c21a61a40e7db26f85336ff0e588dd4ecd71e
Vitaliy <vitalik.koliesnikov@gmail.com>
2023-02-20 23:55:47 +0200

    header and hero fix

--- js/script.js
+++ js/script.js
@@ -3,11 +3,7 @@ const iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
     iconMenu.addEventListener("click", function () {
         document.body.classList.toggle('_lock');   
-        let children = header.children;
-        console.log(children);
-        for(let i=0; i<children.length; i++){
-            children[i].classList.toggle('_active');
-        }
-        iconMenu.classList.toggle('_active');
+        header.classList.toggle('_active');
     });
 }
+
