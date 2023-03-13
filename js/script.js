c6bc7a2e485c360673d7a59b3d6dba8f04a45c52
Vitaliy <vitalik.koliesnikov@gmail.com>
2023-03-13 22:44:15 +0200

    lazyloading were added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@ if (iconMenu) {
 
 
 new Swiper('.first__swiper', {
+    preloadImages: false,
+    lazy:{
+      loadOnTransitionStart: false,
+      loadPrevNext: false,
+    },
     direction: 'horizontal',
     navigation: {
       nextEl: '.swiper-button-next',
@@ -29,6 +34,11 @@ new Swiper('.first__swiper', {
   });
 
   new Swiper('.second__swiper', {
+    preloadImages: false,
+    lazy:{
+      loadOnTransitionStart: false,
+      loadPrevNext: false,
+    },
     direction: 'horizontal',
     navigation: {
       nextEl: '.swiper-button-next',
